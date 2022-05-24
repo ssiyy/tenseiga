a3803810b423ff1ad58d5056bb361cc2a1e4d198
siy <WuGang@inke.cn>
2022-05-24 18:16:46 +0800

    初始版本

---
+++ buildSrc/src/main/java/com/siy/tansaga/TansagaPlugin.kt
@@ -0,0 +1,16 @@
+package com.siy.tansaga
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+/**
+ *
+ * @author  Siy
+ * @since  2022/5/24
+ */
+class TansagaPlugin :Plugin<Project>{
+    override fun apply(p0: Project) {
+
+    }
+}

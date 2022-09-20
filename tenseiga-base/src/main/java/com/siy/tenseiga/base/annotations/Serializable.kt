6836685aa21c94f9209d40a013aa613586c34fc1
siy <WuGang@inke.cn>
2022-09-20 14:43:33 +0800

    添加支持序列化功能

--- tenseiga-base/src/main/java/com/siy/tenseiga/base/annotations/Serializable.kt
+++ tenseiga-base/src/main/java/com/siy/tenseiga/base/annotations/Serializable.kt
@@ -1,7 +1,8 @@
 package com.siy.tenseiga.base.annotations
 
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
+@Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS,AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class Serializable(
-    vararg val value:String
+    vararg val value:String,
+    val withUUid:Boolean = false
 )

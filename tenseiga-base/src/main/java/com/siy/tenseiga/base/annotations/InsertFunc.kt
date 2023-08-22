b17d946c0c2037b598d77dc05a7d196acc6a7b00
siy <WuGang@inke.cn>
2023-08-22 09:50:29 +0800

    修复bug

--- tenseiga-base/src/main/java/com/siy/tenseiga/base/annotations/InsertFunc.kt
+++ tenseiga-base/src/main/java/com/siy/tenseiga/base/annotations/InsertFunc.kt
@@ -1,5 +1,8 @@
 package com.siy.tenseiga.base.annotations
 
+/**
+ * 会在类中插入随机的属性字段和调用空方法
+ */
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 annotation class InsertFunc
